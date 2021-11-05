329588913401e6f36c06639f0ca70ee812c64b6e
Simranpreet Singh <70853270+simranlotey@users.noreply.github.com>
2021-11-05 16:48:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar"
 function App() {
   return (
     <>
-    <Navbar name="Simranpreet Singh"/>
+    <Navbar/>
     <Mobile alter="Card image cap"/>
     </>
   );
